48cd75d53cc04de0652ac34bfbf151105652bf30
Howl1935 <travisphawley@gmail.com>
2022-09-16 23:54:56 -0700

    finished section more statically

---
+++ src/components/Hackathon/Hackathon.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Hackathon = (props) => {
+  const { isFuture, eventName, startDate, endDate, imgUrl } = props.data;
+  return (
+    <div className="flex flex-row text mt-3 mb-3 justify-between  xl:mt-5 xl:mb-5">
+      <div className="hack-img  w-[75px] md:w-[110px] lg:w-[110px] xl:w-[130px] ">
+        <div className="flex relative">
+          {isFuture && <div className="absolute -left-4 h-[75px] md:h-[110px] xl:h-[130px] xl:-left-7 yellow-box"></div>}
+          <img className="rounded-md md:rounded-xl lg:rounded-2xl" src={imgUrl} alt=" of stuff" />
+        </div>
+      </div>
+      <div className="flex flex-col  justify-between md:flex-row md:justify-between md:w-10/12">
+      <div className="flex text-big justify-end order-last md:items-center hack-name text-[18px] md:text-[22px] lg:text-[32px] md:order-first "> {eventName} </div>
+      <div className="flex items-center text-[22px] ">
+        <div className="date-box border-1 rounded-md p-2 text-[16px] md:text-[22px] md:p-3 ">
+          {startDate} <span> {">"} </span> {endDate}
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hackathon;
---
+++ src/data/index.js
@@ -0,0 +1,32 @@
+export const hackathons = [
+  {
+    isFuture: true,
+    eventName: "ETH San Francisco 2022",
+    startDate: "Nov 04, 2022",
+    endDate: "Nov 06, 2022",
+    imgUrl:
+      "https://storage.googleapis.com/ethglobal-api-production/events/441gg/logo/1653491644559_sf-logo.jpg",
+  },
+  {
+    isFuture: false,
+    eventName: "ETH MEXICO 2022",
+    startDate: "Aug 19, 2022",
+    endDate: "Aug 21, 2022",
+    imgUrl: "https://mexico.ethglobal.com/img/illustration.svg",
+  },
+  {
+    isFuture: false,
+    eventName: "ETH CC5 Paris 2022",
+    startDate: "Jul 19, 2022",
+    endDate: "July 21, 2022",
+    imgUrl: "https://pbs.twimg.com/profile_images/725947738781708288/95vIQJR-_400x400.jpg",
+  },
+  {
+    isFuture: false,
+    eventName: "Road to Web3 2022",
+    startDate: "Feb 3, 2022",
+    endDate: "Feb 8, 2022",
+    imgUrl:
+      "https://storage.googleapis.com/ethglobal-api-production/events/5b9mq/logo/1652207364354_roadtoweb3.png",
+  },
+];
