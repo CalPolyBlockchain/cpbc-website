b9907f0c9a0c5cf8b3f220860c6108cd3f73017d
Howl1935 <travisphawley@gmail.com>
2022-09-18 21:35:31 -0700

    work done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,30 +1,47 @@
 import React from "react";
-import { projectHero, cpbc_logo } from "../../images/index";
+import { cpbc_logo } from "../../images/index";
 import './About.css'
 
 const About = () => {
   return (
-    <>
-    <div id="theme" className="relative  wedge-is-on h-[453px] md:h-[437px] lg:h-[700px] xl:h-[700px]">
-    <img
-      className=" absolute object-cover object-center my-44 scale-[2.5] md:scale-[1.0] md:my-0 lg:scale-[1.0] lg:-my-36 xl:scale-70 "
-      src={projectHero}
-      alt="Evening at Morro Rock"
-    />
-    </div>
-    <div className="container-wide flex"> 
-    <div>
-        <img src={cpbc_logo} alt="CP" width={"387px"} />
-    </div>
-    <div className="max-w-xl text">
-        <div>Blockchain at Cal Poly</div>
-        <div>Education | Research | Development</div>
-        <div>Our education team develops in-house content and sources guest speakers from top protocols, such as Ethereum, Chainlink, The Graph, Celo, Near, DyDx, and more!
+    <div className="mb-10">
+      <div className="relative  wedge-is-on h-[453px] md:h-[437px] lg:h-[700px] xl:h-[700px]  w-full ">
+        <div className="about_bg w-full h-full"></div>
+      </div>
 
-Topics range from consensus and DeFi primitives, to being able to spin up your own decentralized exchange! We show students and faculty how easy it is to plug into the DeFi ecosystem.</div>
-    </div>
+      <div className="flex flex-col container-wide  max-w-xl pb-10 text ">
+        <div className="">
+          <div className="flex mb-4">
+            <div className="w-3/12 mr-3">
+              <img src={cpbc_logo} alt="CP" width={"387px"} />
+            </div>
+            <div className="flex flex-col justify-center w-max ">
+              <div className="w-max text-[18px] font-semibold">
+                Blockchain at Cal Poly
+              </div>
+              <div className="w-max text-[14px] font-light">
+                Education | Research | Development
+              </div>
+            </div>
+          </div>
         </div>
-    </>
+        <div className=" text-[12px] font-regular mb-4">
+          Our education team develops in-house content and sources guest
+          speakers from top protocols, such as Ethereum, Chainlink, The Graph,
+          Celo, Near, DyDx, and more!
+        </div>
+        <div className=" text-[12px] font-regular ">
+          Topics range from consensus and DeFi primitives, to being able to spin
+          up your own decentralized exchange! We show students and faculty how
+          easy it is to plug into the DeFi ecosystem.
+        </div>
+
+      </div>
+      <div className="flex container-wide justify-between  ">
+      <button className="text font-bold text-[18px] py-3 px-7  border-2 rounded-md">Contact Us</button>
+      <button className="text font-bold text-[18px] py-3 px-7  border-2 rounded-md">Learn More</button>
+      </div>
+    </div>
   );
 };
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { logo } from '../../images/index'
+import { FaBeer } from 'react-icons/fa';
+const NavBar = ({toggleTheme}) => {
 
-const NavBar = () => {
+
   return (
-    <div className="fixed top-0 flex justify-center text pt-5 z-10 w-full text-[12px] md:text-[18px]  ">
+    <div className="fixed top-0 flex justify-center text-white pt-5 z-10 w-full text-[12px] md:text-[18px]  ">
       <ul className="flex flex-row items-center justify-evenly  container-nar ">
         <li>ABOUT</li>
         <li>PROJECTS</li>
         <li><img class="w-[58px] md:w-[80px] lg:w-[93px]"src={logo} alt="Cal Poly Blockchain Logo"/></li>
         <li>TEAM</li>
         <li>GET INVOLVED</li>
+        <li><FaBeer onClick={()=> toggleTheme()} /></li>
 
 
 
