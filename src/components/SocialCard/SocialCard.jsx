35bffd16ee72f2352adfc63b2f5b1d5c30ce958e
Howl1935 <travisphawley@gmail.com>
2023-01-24 14:45:30 -0800

    Accidentally pushed just server

--- src/components/SocialCard/SocialCard.jsx
+++ src/components/SocialCard/SocialCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 
 const SocialCard = (props) => {
-  const {companyName, count, icon} = props.data  
+  const {companyName,  icon} = props.data  
+  const discordFollowers  = props.discord
+  const twitterFollowers  = props.twitter
   return (
     <div className='flex flex-row'>
-        <div className="flex items-center text-9xl">
+        <div className="flex items-center text-[14rem] sm:text-9xl pl-[10px] sm:pr-[20px]">
             {icon}
         </div>
-        <div> 
-            <div className="text-6xl font-bold">{ companyName } </div>
-            <div className="text-8xl font-bold text-right">{ count }</div>
+        <div class="flex flex-col justify-center items-center w-full"> 
+            <div className="text-2xl lg:text-6xl font-bold">{ companyName } </div>
+            <div className=" text-7xl lg:text-8xl font-bold text-right">{ companyName.toLowerCase() === "twitter" ? twitterFollowers : discordFollowers }</div>
         </div>
 
 
--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,18 +1,51 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import SocialCard from "../SocialCard/SocialCard";
 import './Socials.css'
 import { socials } from "../../data";
+import axios from 'axios'
+
 
 const Socials = () => {
+
+
+
+  const userId = 1419372755514839041;
+  const server = 'KtNtWd2akR' 
+  const twitter = `https://api.twitter.com/2/users/${userId}`;
+  const bearerToken =process.env.REACT_APP_BEARER_TOKEN;
+  const url1 = '/api'
+  const discord = `https://discord.com/api/v9/invites/${server}?with_counts=true&with_expiration=true`
+  
+
+const bodyParameters = {
+  'user.fields': "public_metrics"
+};
+   const [discordFollowers, setDiscordFollowers] = useState(0);
+   const [twitterFollowers, setTwitterFollowers] = useState(0);
+
+  useEffect(() => {
+    const fetchDiscordData = async () => {
+      const result = await axios.get(discord);
+      setDiscordFollowers(result.data.approximate_member_count);
+    };
+    const fetchTwitterData = async () => {
+      const result = await axios.get('/api');
+      console.log(result)
+      setTwitterFollowers(result.data.data.public_metrics.followers_count);
+    };
+
+   fetchDiscordData();
+   fetchTwitterData();
+  }, []);
+
   return (
     <div id="theme" className="banner  wedge-is-on-social ">
-        <div className="flex flex-row container-mid justify-center">
-
-   
+        <div className="flex flex-col md:flex-row container-mid justify-between pt-[50px] pb-[50px] md:pt-[100px] md:pb-[100px]">
       {socials.map((element) => {
-        return <SocialCard data={ element } />;
+        return <SocialCard data={ element } twitter={twitterFollowers} discord={discordFollowers} />;
       })}
            </div>
+          
     </div>
   );
 };
