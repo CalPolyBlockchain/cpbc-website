6514d5d418241487014abc7cf0a29b1d8dd8d94d
Howl1935 <travisphawley@gmail.com>
2023-01-24 16:33:55 -0800

    all done

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,15 +1,16 @@
 import React from "react";
 import PartnersItem from "../PartnersItem/PartnersItem";
 import { partnersImages} from "../../images";
-
+import './Partners.css'
 const Partners = () => {
+  const links = ["https://ceng.calpoly.edu/", "https://decrypt.co/", "https://www.calpoly.edu/orfalea-college-of-business"  ]
   return (
-    <div class="background object-cover">
-      <div class="container-wide text-center font-semibold items-center text-med text-[29px] lg:text-[38px] text-[#FFFFFF]">
+    <div class="background h-[800px] wedge-partners pt-[20px]" >
+      <div class="container-wide pt-[50px] text-center font-semibold items-center text-med text-[29px] lg:text-[38px] lg:pt-[100px] text-[#FFFFFF]">
         <h2> Our Partners</h2>
-        <div class="flex flex-col mt-3 space-y-8 md:space-y-0 md:align-top lg:align-top md:flex-row md:space-y-0 lg:flex-row">
+        <div class="flex flex-col mt-3 space-y-8 items-center md:space-y-0 md:align-top lg:pt-[75px] lg:align-top md:flex-row md:space-y-0 lg:flex-row">
           {partnersImages.map((element, index) => {
-            return <PartnersItem img={element} index={index} />;
+            return <PartnersItem img={element} index={index} link={links[index]} />;
           })}
         </div>
       </div>
--- src/components/PartnersItem/PartnersItem.js
+++ src/components/PartnersItem/PartnersItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 const PartnersItem = (props) => {
     
   return (
-    <div class="m-2 md:w-[900px] lg:w-[900px] xl:w-[900px] 2xl:w[900px]">
+    <div class="m-2 md:w-[900px] lg:w-[900px] xl:w-[900px] 2xl:w[900px]" style={{ cursor: 'pointer' }} onClick={() => window.open(props.link)}>
       <img class="rounded-2xl" src={props.img} alt=" of stuff" />
     </div>
   )
