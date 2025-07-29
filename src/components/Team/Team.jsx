a5abd07ca238b7f1e9efe5d50ea6c310426bd635
Your GitHub Username <your-email@example.comlmckeown>
2025-07-29 15:17:34 -0700

    Increase image height to 45vh and fix vertical alignment to side-by-side layout

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -45,15 +45,13 @@ const Team = () => {
             <div class='flex flex-col md:flex-row pt-2 pb-10 md:pt-10 md:pb-10 order-1 md:order-2 w-full'>
               <div className='w-full md:w-1/2'>
 
-              <div className='w-full md:w-full pl-2 pr-2 md:pl-10 md:pr-0 '>
+              <div className='flex flex-row gap-4 w-full md:w-full pl-2 pr-2 md:pl-10 md:pr-0 '>
 
               <img
                   src={teamGroupB}
                   alt='example'
                   className='alumni-image rounded-2xl '
                 />
-                </div>
-                <div className='w-full md:w-full pt-5 pl-2 pr-2 md:pl-10 md:pr-0 '>
                 <img
                   src={teamGroupA}
                   alt='example'
