e09cac73367d61c8c59b0f06d5d127e3a9e1cf31
Your GitHub Username <your-email@example.comlmckeown>
2025-07-29 21:39:40 -0700

    Add proper ID and scroll margin for Socials section

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -4,7 +4,7 @@ import { socials } from '../../data';
 
 const Socials = () => {
   return (
-    <div id='theme' className='banner'>
+    <div id='socials' className='banner'>
       <div className='flex flex-col md:flex-row container-mid justify-between pt-[50px] pb-[50px] md:pt-[100px] md:pb-[100px] text-flip'>
         {socials.map((element) => {
           return (
